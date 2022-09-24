e8f312d0b2a532667d4e2153b86c62313640f21a
modernpacifist <modernpacifist@gmail.com>
2022-09-25 01:00:01 +0300

    25/09/2022-01:00:01 automated backup commit

--- src/python_pro_module_1_final_task.py
+++ src/python_pro_module_1_final_task.py
@@ -1,28 +1,26 @@
 #!/bin/env python3.9
-"""Игра угадай число
-Компьютер сам загадывает и сам угадывает число
-"""
 
 import numpy as np
 import statistics
+
 
 def random_predict(number:int=np.random.randint(1, 101)) -> int:
     count = 0
     lst_num = list(range(1, 101))
 
     while True:
-      count += 1
-      predict_number = int(np.mean(lst_num))
+        count += 1
+        predict_number = int(np.mean(lst_num))
 
-      half = round(int(len(lst_num))/2)
-      if number == predict_number:
-        break
-      elif predict_number > number:
-        lst_num = lst_num[half:]
-      else:
-        lst_num = lst_num[:half]
-      if len(lst_num) == 0:
-        break
+        half = round(int(len(lst_num))/2)
+        if number == predict_number:
+          break
+        elif predict_number > number:
+          lst_num = lst_num[half:]
+        else:
+          lst_num = lst_num[:half]
+        if len(lst_num) == 0:
+            break
 
     return count
 
